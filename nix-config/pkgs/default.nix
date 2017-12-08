2b3e36d4d9af900c6026acbebbc64a60889aac2b
Michael Bock <me@thought2.de>
2017-12-08 05:56:56 +0100

    init

---
+++ nix-config/pkgs/default.nix
@@ -0,0 +1,56 @@
+with import <nixpkgs> {};
+
+let
+
+  pkgs =  import <nixpkgs> {};
+
+  more = pkgs.emacsPackagesNg.trivialBuild {
+    pname = "my-mode";
+    src = ./default.el;
+  };
+
+  emacs' = pkgs.emacsWithPackages (epkgs: with epkgs; [
+    #elm-oracle
+    aggressive-indent
+    auto-complete
+    better-defaults
+    cider
+    clojure-mode
+    company
+    company
+    elm-mode
+    flycheck
+    flycheck-elm
+    #melpaPackages.nix-mode
+    mmm-mode
+    more
+    paredit
+    smartparens
+    tide
+    web-mode
+  ]);
+
+  mkAlias = name: command: writeScriptBin name ''
+    #!${bash}/bin/bash
+    ${command} "$@"
+  '';
+
+  bashInitFile = ''
+
+  '';
+
+  aliases = {
+    emacs = "${emacs'}/bin/emacs --no-splash";
+    emacs-vanilla = "${emacs}/bin/emacs";
+    bash = "${bash}/bin/bash --init-file ${bashInitFile}";
+  };
+  
+  aliaspkgs = builtins.attrValues (lib.mapAttrs mkAlias aliases);
+
+in [
+  aliaspkgs
+] ++ (with elmPackages; [
+  elm
+]) ++ (with nodePackages; [
+  node2nix
+])
