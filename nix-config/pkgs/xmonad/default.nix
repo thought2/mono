3a1e77657e0ad57938f225bd049e4cab04c70fa9
Michael Bock <me@thought2.de>
2017-12-25 10:52:03 +0100

    Some XMonad config. e.g. Virtual Screens

--- nix-config/pkgs/xmonad/default.nix
+++ nix-config/pkgs/xmonad/default.nix
@@ -5,30 +5,56 @@
   ]
 }:
 let
-  config = ''
-    import XMonad
+  temp = /* clojure */ ''
+    (def x 33) 
+  '';
+  config = /* haskell */ ''
+    import XMonad hiding ( (|||) )
     import XMonad.Util.EZConfig(additionalKeys)
+    import XMonad.Layout.NoBorders
+    import XMonad.Layout.LayoutCombinators
     --import XMonad.Hooks.DynamicLog
     --import XMonad.Hooks.ManageDocks
     --import XMonad.Util.Run(spawnPipe)
     --import System.IO
+    import XMonad.Layout.LayoutScreens
+    import XMonad.Layout.TwoPane
 
-
+    --import XMonad.Layout.Gaps
+    import XMonad.Layout.Spacing
+    
     main = do
       xmonad $ defaultConfig
         { focusedBorderColor = "#fc7474"
-        , borderWidth        = 2
+        , borderWidth        = 4
         , modMask            = mod4Mask
-        } `additionalKeys` myKeys
+        , layoutHook         = layout
+        } `additionalKeys` shortcuts
 
-    myKeys =
-      [ ((mod4Mask, xK_f),      spawn "${pkgs.firefox}/bin/firefox")
-      , ((mod4Mask, xK_c),      spawn "${pkgs.chromium}/bin/chromium-browser")
-      , ((mod4Mask, xK_a),      spawn "${pkgs.chromium}/bin/chromium-browser --app='http://ddg.gg'")
-      , ((mod4Mask, xK_e),      spawn "${pkgs.emacs}/bin/emacs")
-      , ((mod4Mask, xK_Return), spawn "${pkgs.xterm}/bin/xterm")
-
+    shortcuts =
+      [ ((mod .|. ctrl,   xK_f),             spawn "${pkgs.firefox}/bin/firefox")                                
+      , ((mod .|. ctrl,   xK_c),             spawn "${pkgs.chromium}/bin/chromium-browser")                      
+      , ((mod .|. ctrl,   xK_a),             spawn "${pkgs.chromium}/bin/chromium-browser --app='http://ddg.gg'")
+      , ((mod .|. ctrl,   xK_e),             spawn "${pkgs.emacs}/bin/emacs")                                    
+      , ((mod .|. ctrl,   xK_Return),        spawn "${pkgs.xterm}/bin/xterm")
+      , ((mod .|. ctrl,   xK_f),             sendMessage $ JumpToLayout "Full")
+      
+      , ((mod .|. shift,                     xK_space), virtualScreens)
+      , ((mod .|. ctrl .|. shift, xK_space), rescreen)
       ]
+      where
+        virtualScreens = layoutScreens 2 $ spacingWithEdge 10 $ TwoPane 0.55 0.45
+        mod            = mod4Mask
+        ctrl           = controlMask
+        shift          = shiftMask
+        
+    
+
+    layout = tall ||| tall2 ||| full
+      where
+        tall  = Tall 1 (3/100) (1/2)
+        tall2 = Mirror tall 
+        full  = noBorders Full
   '';
 in
 pkgs.stdenv.mkDerivation {
