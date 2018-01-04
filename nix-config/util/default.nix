a32f171611b3b8f246702a7a44348f3449325435
Michael Bock <me@thought2.de>
2018-01-04 23:47:15 +0100

    Made mkAlias more generic for use of $1, $2, etc..

--- nix-config/util/default.nix
+++ nix-config/util/default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 rec {
   mkAlias = name: command: writeScriptBin name ''
     #!${bash}/bin/bash
-    ${command} "$@"
+    ${command}
   '';
 
   mkAliases = def: builtins.attrValues (lib.mapAttrs mkAlias def);
