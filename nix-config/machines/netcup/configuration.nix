a0116aafbc20310bf15d619d945c04ee7d6d4116
Michael Bock <me@thought2.de>
2018-01-07 19:59:01 +0100

    Implemented trivial revision index for webserver.

--- nix-config/machines/netcup/configuration.nix
+++ nix-config/machines/netcup/configuration.nix
@@ -1,5 +1,32 @@
-{pkgs, ...}: {
+{pkgs, ...}:
+with import ../../util;
+with builtins;
+with pkgs;
+let
 
+  revisions = [
+    {
+      #rev = "";
+      sha256 = "1pb7vljk8zdnxpk7cy8k6y2d9w102kz1ziz921pcivwbkxlfd10l";
+    }
+  ];
+
+  mkOut = i: x: {
+    urlPath = "/video/${toString i}";
+    dir = import (pkgs.fetchgit {
+      url = "git://46.38.233.235:/video.git";
+      sha256 = x.sha256;
+    });
+  };
+  
+  dirs = mapIndexed mkOut revisions;
+
+  overview = mapIndexed (i: x: {
+    urlPath = x.urlPath;
+    index = i;
+  }) dirs;
+in
+{
         
   services.httpd = {
     enable       = true;
@@ -7,7 +34,17 @@
     documentRoot = pkgs.runCommand "cmd" {} ''
       mkdir $out
       echo "test output" > $out/index.txt
-    '';    
+    '';
+    servedFiles = [
+      {
+        urlPath = "/video";
+        file = writeText "index.html" ''
+          <h1>Revision Index</h1>
+          ${concatStringsSep "\n" (map (x: "<a href=${x.urlPath}>Revision ${toString x.index}</a>") overview)}
+        '';
+      }
+    ];
+    servedDirs = [] ++ dirs;
   };
 
   networking.firewall.allowedTCPPorts = [ 80 ];  
