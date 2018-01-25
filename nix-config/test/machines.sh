61fc81410c65b50e24baf32562fe98b51b1d8411
Michael Bock <me@thought2.de>
2018-01-25 21:56:02 +0100

    improved machine test

--- nix-config/test/machines.sh
+++ nix-config/test/machines.sh
@@ -2,11 +2,42 @@
 
 MACHINES_DIR=$CONFIG_DIR/machines
 
-echo -e "Building virtual machines of all configurations.\n"
+CONFIG_FILES=($MACHINES_DIR/*/configuration.nix)
 
-for file in $MACHINES_DIR/*/configuration.nix
+SEP1="=====================================================================\n"
+SEP2="---------------------------------------------------------------------\n"
+
+RESULTS=()
+
+# Header
+
+echo -e "Building virtual machines from the following files:\n"
+
+for file in "${CONFIG_FILES[@]}"
+  do
+    echo $file
+  done
+
+echo -e $SEP1
+
+
+# Body
+
+i=0
+
+for file in "${CONFIG_FILES[@]}"
   do
     echo -e "Building VM for machine in file $file\n"
     export NIXOS_CONFIG=$file; nixos-rebuild build-vm;
-    echo -e "---------------------------------------------------------------------\n"
+    RESULTS[i]=$?
+    echo -e $SEP2
+
+    i=$i+1
   done
+
+echo -e $SEP1
+
+
+# Footer
+
+#TODO: Print results
