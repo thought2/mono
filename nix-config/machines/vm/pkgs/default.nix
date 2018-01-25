9062c5edcfe1a8ad903e03e6d5dc46121381dda0
Michael Bock <me@thought2.de>
2018-01-25 09:43:47 +0100

    Added hq-patch-node-bins

--- nix-config/machines/vm/pkgs/default.nix
+++ nix-config/machines/vm/pkgs/default.nix
@@ -3,6 +3,15 @@ with import ../../../util;
 { pkgs }:
 let
   aliases = {
+    hq-patch-node-bins = ''
+
+      rm node_modules/optipng-bin/vendor/optipng
+      ln -s ${getBin pkgs.optipng "optipng"} node_modules/optipng-bin/vendor/optipng
+
+      rm node_modules/pngquant-bin/vendor/pngquant
+      ln -s ${getBin pkgs.pngquant "pngquant"} node_modules/pngquant-bin/vendor/pngquant
+
+    '';
   };
 in
 mkAliases aliases
