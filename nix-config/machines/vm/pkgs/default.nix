80d87c72a47bf8d85147b63c1b89a0bc0953a43a
Michael Bock <me@thought2.de>
2018-01-26 13:24:19 +0100

    Added HQ shorthands

--- nix-config/machines/vm/pkgs/default.nix
+++ nix-config/machines/vm/pkgs/default.nix
@@ -13,6 +13,27 @@ let
       ln -s ${bin pkgs.pngquant "pngquant"} node_modules/pngquant-bin/vendor/pngquant
 
     '';
+
+    hq-start = withPath [ pkgs.yarn ] "yarn start";
+
+    hq-start-beta = withPath [ pkgs.yarn ] ''
+      export API_DOMAIN=hqngin-beta.hqrevenue.com
+      echo $API_DOMAIN
+      yarn start
+    '';
+
+    hq-start-release = withPath [ pkgs.yarn ] ''
+      export API_DOMAIN=hqngin-rc.hqrevenue.com
+      echo $API_DOMAIN
+      yarn start
+    '';
+
+    hq-start-production = withPath [ pkgs.yarn ] ''
+      export API_DOMAIN=hqngin.hqrevenue.com
+      echo $API_DOMAIN
+      yarn start
+    '';
+
   };
 in
 mkAliases aliases
