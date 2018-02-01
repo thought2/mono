cb27ba7d123f2585215f558e206ddc7e07b2bc58
Michael Bock <me@thought2.de>
2018-02-01 15:57:56 +0100

    + unclutter

--- nix-config/machines/macbook2013/configuration.nix
+++ nix-config/machines/macbook2013/configuration.nix
@@ -1,17 +1,28 @@
+with import <nixpkgs> {};
+
+let
+  localPkgs = import ./pkgs { inherit pkgs; };
+in
 {
   imports = [
     ./hardware-configuration.nix
     ../../modules/laptop.nix
   ];
-  
+
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
+
+  hardware.bluetooth.enable = true;
+
+  services.unclutter.enable = true;
 
   networking.networkmanager.enable = true;
 
   networking.enableB43Firmware = true;
 
   nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = localPkgs;
 }
