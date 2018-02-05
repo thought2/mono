d20699944250ea9fe5e919716329c0ab92aaac1d
Michael Bock <me@thought2.de>
2018-02-05 17:29:24 +0100

    - hq modules from vm pkgs

--- nix-config/machines/vm/pkgs/default.nix
+++ nix-config/machines/vm/pkgs/default.nix
@@ -4,36 +4,6 @@ with import ../../../util;
 let
   aliases = {
 
-    hq-patch-node-bins = ''
-
-      rm node_modules/optipng-bin/vendor/optipng
-      ln -s ${bin pkgs.optipng "optipng"} node_modules/optipng-bin/vendor/optipng
-
-      rm node_modules/pngquant-bin/vendor/pngquant
-      ln -s ${bin pkgs.pngquant "pngquant"} node_modules/pngquant-bin/vendor/pngquant
-      
-    '';
-
-    hq-start = withPath [ pkgs.yarn ] "yarn start";
-
-    hq-start-beta = withPath [ pkgs.yarn ] ''
-      export API_DOMAIN=hqngin-beta.hqrevenue.com
-      echo $API_DOMAIN
-      yarn start
-    '';
-
-    hq-start-release = withPath [ pkgs.yarn ] ''
-      export API_DOMAIN=hqngin-rc.hqrevenue.com
-      echo $API_DOMAIN
-      yarn start
-    '';
-
-    hq-start-production = withPath [ pkgs.yarn ] ''
-      export API_DOMAIN=hqngin.hqrevenue.com
-      echo $API_DOMAIN
-      yarn start
-    '';
-
   };
 in
 mkAliases aliases
