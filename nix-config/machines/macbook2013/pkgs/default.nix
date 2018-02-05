b62f501cf917501b935960c22c7c6233b08be6ce
Michael Bock <me@thought2.de>
2018-02-05 17:21:05 +0100

    Changed screen configuration.

--- nix-config/machines/macbook2013/pkgs/default.nix
+++ nix-config/machines/macbook2013/pkgs/default.nix
@@ -6,33 +6,31 @@ let
   screens = {
     native = "eDP1";
     main = "HDMI3";
-    side = "DP2";
+    side = "DP1";
   };
 
   aliases = {
 
     screens-1 = with screens; withPath [pkgs.xorg.xrandr] ''
 
-      xrandr --output ${native} --mode 1440x900 --primary \
+      xrandr --output ${native} --mode 1440x900 \
              --output ${main} --off \
-             --output ${side} --off
+             --output ${side} --off \
     '';
 
     screens-2 = with screens; withPath [pkgs.xorg.xrandr] ''
 
       xrandr --output ${native} --off \
              --output ${main} --primary --auto --right-of ${native} \
-             --output ${side} --auto --right-of ${main}
+             --output ${side} --auto --right-of ${main} \
     '';
 
     screens-3 = with screens; withPath [pkgs.xorg.xrandr] ''
 
-      xrandr --noprimary --output ${native} --mode 1440x900 \
+      xrandr --output ${native} --mode 1440x900 \
              --output ${main} --auto --right-of ${native} \
-             --output ${side} --auto --right-of ${main}
+             --output ${side} --auto --right-of ${main} \
     '';
-
-
 
   };
 in
