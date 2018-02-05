bd6f51c1254a6ec1680060663f96c7478b70ab2f
Michael Bock <me@thought2.de>
2018-02-05 17:27:39 +0100

    + sound card config

--- nix-config/machines/macbook2013/configuration.nix
+++ nix-config/machines/macbook2013/configuration.nix
@@ -25,4 +25,21 @@ in
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = localPkgs;
+
+  environment.etc = {
+    "asound.conf".source = pkgs.writeText "asound.conf" ''
+
+      pcm.!default {
+        type hw
+        card 1
+      }
+
+      ctl.!default {
+        type hw
+        card 1
+      }
+
+    '';
+  };
+
 }
