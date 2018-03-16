bcb81f896acafe07840e8b9a0d3a37ef3b624267
Michael Bock <me@thought2.de>
2018-03-16 13:31:48 +0100

    Refactor kworker fix

--- nix-config/machines/macbook2013/configuration.nix
+++ nix-config/machines/macbook2013/configuration.nix
@@ -46,12 +46,18 @@ in
     '';
   };
 
-
-  # see https://sudoremember.blogspot.de/2013/05/high-cpu-usage-due-to-kworker.html
-  services.cron.systemCronJobs =
-    [ "@reboot echo disable > /sys/firmware/acpi/interrupts/gpe06"
-    ];
-
   nixpkgs.overlays = [ overlays ];
 
-}
+} //
+# see https://sudoremember.blogspot.de/2013/05/high-cpu-usage-due-to-kworker.html
+(let
+  fix-kworker = pkgs.writeShellScriptBin "fix-kworker" ''
+    echo disable > /sys/firmware/acpi/interrupts/gpe06
+  '';
+in
+{
+  services.cron.systemCronJobs =
+    [ "@reboot root ${fix-kworker}/bin/fix-kworker"];
+
+  environment.systemPackages = [ fix-kworker ];
+})
