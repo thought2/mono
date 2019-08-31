d83f33bd9d150b83f6dc6e8d2b47f58d8a40f898
Michael Bock <me@thought2.de>
2019-08-31 08:55:20 +0200

    Change stage

--- nix-config/hosts/stage.nix
+++ nix-config/hosts/stage.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
 {
   imports =
-    [
+    [ ../machines/netcup/configuration.nix
+      ../modules/ssh.nix
     ];
 
+  networking.hostName = "stage";
+
+  system.stateVersion = "18.09";
+
+  hardware.cpu.intel.updateMicrocode = true;
 }
