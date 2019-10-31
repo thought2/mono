97a4393abb75a22905446b53dfb503d25cda240a
Michael Bock <me@thought2.de>
2019-10-31 07:47:17 +0100

    Add xmonad custom project

---
+++ xmonad-custom/default.nix
@@ -0,0 +1,23 @@
+{ mkDerivation, aeson, base, bytestring, hpack, stdenv, xmonad
+, xmonad-contrib
+}:
+mkDerivation {
+  pname = "xmonad-custom";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson base bytestring xmonad xmonad-contrib
+  ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    aeson base bytestring xmonad xmonad-contrib
+  ];
+  testHaskellDepends = [
+    aeson base bytestring xmonad xmonad-contrib
+  ];
+  preConfigure = "hpack";
+  homepage = "https://github.com/githubuser/xmonad-custom#readme";
+  license = stdenv.lib.licenses.bsd3;
+}
