2e379f563c603b8bbaa23e46b49af250b5798f21
Michael Bock <me@thought2.de>
2019-11-20 13:39:02 +0100

    Finish hot reload

--- purescript-hot-reload/src/HotReload.purs
+++ purescript-hot-reload/src/HotReload.purs
@@ -1,104 +1,80 @@
-module HotReload where
+module HotReload (init, saveSnapshot, defaultInitConfig, InitConfig, module HotReload.IdRef) where
 
 import Prelude
 import Data.Argonaut (class DecodeJson, class EncodeJson, Json, decodeJson, encodeJson)
 import Data.Array as Array
-import Data.Default (class Default, def)
 import Data.Either as Either
 import Data.Maybe (Maybe(..), maybe)
-import Data.Newtype (class Newtype)
-import Data.Unit (Unit)
-import Effect (Effect)
 import Effect.Console as Console
+import Effect.Unsafe (unsafePerformEffect)
+import HotReload.IdRef (Id(Id))
 import HotReload.IdRef as IdRef
-import Partial.Unsafe (unsafeCrashWith)
 
 newtype MigrateFn
   = MigrateFn (Array Json -> Json -> Json)
 
-newtype Id
-  = Id IdRef.Id
+-- HOT RELOAD
+type InitConfig
+  = { cache :: Int
+    , migrateFn :: { previous :: Array Json, current :: Json } -> Json
+    }
 
--- INIT
+defaultInitConfig :: InitConfig
+defaultInitConfig =
+  { cache: 10
+  , migrateFn:
+    \{ previous, current } ->
+      maybe current identity (Array.last previous)
+  }
+
 type Cell
   = { history :: Array Json
     , snapshot :: Unit -> Json
     }
 
+-- INIT
 init ::
   forall a.
   EncodeJson a =>
   DecodeJson a =>
-  { id :: Id, migrateState :: MigrateFn } -> a -> Effect a
-init { id: Id id, migrateState: MigrateFn migrateFn } initState = do
-  Console.log "xxx"
-  res :: Maybe Cell <- IdRef.read id
-  case res of
-    Nothing -> do
-      IdRef.write id
-        { history: []
-        , snapshot: \_ -> encodeJson initState
-        }
-      Console.log "a"
-      pure initState
-    Just { history, snapshot } -> do
-      let
-        newHistory = Array.snoc history (snapshot unit)
-      _ <- IdRef.modify (_ { history = newHistory }) id
-      let
-        newSnapshot = migrateFn newHistory (encodeJson initState)
+  Id -> InitConfig -> a -> a
+init id { cache, migrateFn } initState =
+  unsafePerformEffect
+    $ do
+        res :: Maybe Cell <- IdRef.read id
+        case res of
+          Nothing -> do
+            IdRef.write id
+              { history: []
+              , snapshot: \_ -> encodeJson initState
+              }
+            pure initState
+          Just { history, snapshot } -> do
+            let
+              newHistory = Array.snoc history (snapshot unit)
+            _ <- IdRef.modify (_ { history = newHistory }) id
+            let
+              newSnapshot =
+                migrateFn
+                  { previous: newHistory
+                  , current: encodeJson initState
+                  }
 
-        newX :: Maybe a
-        newX = Either.hush $ decodeJson newSnapshot
-      Console.log "b"
-      case newX of
-        Nothing -> do
-          _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson initState }) id
-          Console.log "State transition failed"
-          pure initState
-        Just x -> do
-          _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson x }) id
-          pure x
+              newX :: Maybe a
+              newX = Either.hush $ decodeJson newSnapshot
+            case newX of
+              Nothing -> do
+                _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson initState }) id
+                Console.log "State transition failed"
+                pure initState
+              Just x -> do
+                _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson x }) id
+                pure x
 
-{-
-  res :: Maybe Cell <- IdRef.read id
-  case res of
-    Nothing -> do
-      IdRef.write id
-        { history: []
-        , snapshot: \_ -> encodeJson initState
-        }
-      pure initState
-    Just { history, snapshot } -> do
-      let
-        newHistory = Array.snoc history (snapshot unit)
-      _ <- IdRef.modify (_ { history = newHistory }) id
-      let
-        newSnapshot = migrateFn newHistory (encodeJson initState)
-
-        newX = Either.hush $ decodeJson newSnapshot
-      case newX of
-        Nothing -> do
-          _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson initState }) id
-          Console.log "State transition failed"
-          pure initState
-        Just x -> do
-          _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson x }) id
-          pure x
--}
 -- SAVE SNAPSHOT
-saveSnapshot :: forall a. EncodeJson a => { id :: Id } -> a -> Effect Unit
-saveSnapshot { id: Id id } x = do
-  _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson x }) id
-  pure unit
-
--- INSTANCES
-instance defaultMigrateFn :: Default MigrateFn where
-  def = MigrateFn $ \prev curr -> maybe curr identity (Array.last prev)
-
-instance defaultId :: Default Id where
-  def = Id (IdRef.Id "appState")
-
-derive instance newtypeMigrateFn :: Newtype MigrateFn _
-
-derive instance newtypeId :: Newtype Id _
+saveSnapshot :: forall a. EncodeJson a => Id -> a -> a
+saveSnapshot id x =
+  unsafePerformEffect
+    $ do
+        _ <- IdRef.modify (_ { snapshot = \_ -> encodeJson x }) id
+        pure x
