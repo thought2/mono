1101d90a02bf09bf960d04290b47b359c5154822
Michael Bock <me@thought2.de>
2019-11-24 06:53:03 +0100

    Fix pinned-args.nix

--- fad9-cli/nix/pinned-args.nix
+++ fad9-cli/nix/pinned-args.nix
@@ -1,10 +1,9 @@
 let
-  sources = import ./niv/sources.nix;
+  sources = import ./sources.nix;
   nixpkgs = import sources.nixpkgs { };
   spago2nix = import sources.spago2nix { };
 in nixpkgs // {
   pkgs = nixpkgs;
   node2nix = nixpkgs.nodePackages.node2nix;
-  parcel-bundler = nixpkgs.nodePackages.parcel-bundler;
   inherit spago2nix;
 }
