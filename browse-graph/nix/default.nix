68e176572a27335ab592d67c4852764ae1969092
Michael Bock <me@thought2.de>
2019-11-29 13:48:58 +0100

    Init project

---
+++ browse-graph/nix/default.nix
@@ -0,0 +1,42 @@
+{ stdenv, spago, purescript, niv, nix, writeShellScriptBin, spago2nix
+, runCommand, pkgs, parcel-bundler, writeText }:
+let
+  generateNix = writeShellScriptBin "generate-nix" ''
+    ${spago2nix}/bin/spago2nix generate
+    mkdir -p nix/spago2nix
+    mv spago-packages.nix nix/spago2nix
+  '';
+
+  buildPureScript = rec {
+    spagoPkgs = import ./spago2nix/spago-packages.nix { inherit pkgs; };
+    projectOutput = spagoPkgs.mkBuildProjectOutput {
+      src = ../src;
+      purs = purescript;
+    };
+  };
+
+  bundle = let
+    entry = writeText "index.js" ''
+      var Main = require("./output/Main")
+      Main.main()
+    '';
+  in runCommand "bundle" { } ''
+    TMP=`mktemp -d`
+    ln -s ${entry} $TMP/index.js
+    ln -s ${buildPureScript.projectOutput}/output $TMP/output
+    mkdir $out
+    ${parcel-bundler}/bin/parcel \
+      build \
+      --out-dir $out \
+      $TMP/index.js \
+  '';
+
+in stdenv.mkDerivation {
+  name = "browse-graph";
+  buildInputs = [ spago purescript niv.niv generateNix parcel-bundler ];
+  buildCommand = ''
+    mkdir $out
+    cp ${bundle}/index.js $out/index.js
+    cp -r ${../assets}/* -t $out
+  '';
+}
---
+++ browse-graph/nix/pinned-args.nix
@@ -0,0 +1,14 @@
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs = import sources.nixpkgs { };
+  easy-purescript-nix = import sources.easy-purescript-nix { };
+  spago2nix = import sources.spago2nix { };
+  niv = import sources.niv { };
+in {
+  inherit (nixpkgs) stdenv nix writeShellScriptBin runCommand writeText;
+  pkgs = nixpkgs;
+  parcel-bundler = nixpkgs.nodePackages.parcel-bundler;
+  inherit (easy-purescript-nix) spago purescript;
+  inherit spago2nix;
+  inherit niv;
+}
